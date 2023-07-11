db60f001b10ebdaa22c273e1a8dfbc3a6b60343d
Tonius <toniuswar@gmail.com>
2023-07-11 16:31:12 +0300

    new route

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = exp();
 require('dotenv').config();
 const PORT = process.env.PORT;
 const products = require('./routes/products.js');
-const images = require('./routes/cats_images.js')
+const images = require('./routes/cats_images.js');
 
 app.get("/", (req, res)=>{
     res.send('<h1>Start page</h1>')
 });
 app.get("/products", products);
-app.get('/products/:productID', products)
+app.get('/products/:productID', products);
+app.get("/meow", images);
 
 app.listen(PORT, ()=>{
     console.log("Server has been started on port: " + PORT);
