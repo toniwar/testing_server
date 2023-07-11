b578477b218bb0d47a347430aeaf91be7b8213ac
Tonius <toniuswar@gmail.com>
2023-07-11 16:54:04 +0300

    Добавлены котейки

--- routes/cats_images.js
+++ routes/cats_images.js
@@ -24,6 +24,31 @@ const images = [
     {
         id:5,
         url:"https://image.winudf.com/v2/image1/Y29tLndhbGxwYXBlci53YWxscGFwZXJzLktpdHRlbnNDYXRzQ3V0ZVdhbGxwYXBlcnNhbmRCYWNrZ3JvdW5kc19zY3JlZW5fMTNfMTU5ODIzNDYxNF8wMDA/screen-5.jpg?fakeurl=1&type=.jpg"
+    },
+
+    {
+        id:6,
+        url:"https://puzzlik.ru/image/cache/catalog/pazly-puzzlik/castorland-puzzle/500-el/pazly-castorland-kotenok-v-sadu-500-el-b-52974-1-1000x1000.jpg"
+    },
+
+    {
+        id:7,
+        url:"https://krasivosti.pro/uploads/posts/2022-05/1653047141_23-krasivosti-pro-p-bolshie-koshki-dikie-krasivo-foto-31.jpg"
+    },
+
+    {
+        id:8,
+        url:"https://sun9-26.userapi.com/impg/8Q2nJS4OqSYfWUcM6zTg-Jn5VEfyOdUUbICyoA/GwTTVrnW2K4.jpg?size=1280x1280&quality=95&sign=83d09eb8aca199a8338afd61186f85a9&c_uniq_tag=8wAgsB3G90InA5wJhA04U_oX4f6NP1JNldlxXfygGWs&type=album"
+    },
+
+    {
+        id:9,
+        url:"https://s1.1zoom.ru/big3/126/Leopards_Big_cats_Glance_478609.jpg"
+    },
+
+    {
+        id:10,
+        url:"https://img2.goodfon.ru/wallpaper/nbig/9/58/tigr-amurskiy-koshka-morda-7705.jpg"
     }
 
 
